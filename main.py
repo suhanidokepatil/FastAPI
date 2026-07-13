6b6ff9b5fb77b359427551a1046f386e36b38890
suhanidokepatil <suhanidokepatil2005@gmail.com>
2026-07-14 01:00:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,190 +1,161 @@
-from fastapi import FastAPI, Path, HTTPException
-from typing import Optional
-from pydantic import BaseModel
+from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal, Optional
+import json
 
 app = FastAPI()
 
+class Patient(BaseModel):
 
-# Database (temporary dictionary)
-students = {
-    1: {
-        "name": "Suhani",
-        "age": 21,
-        "Qualification": "Engg",
-        "password": "12345"
-    },
-    2: {
-        "name": "Rahul",
-        "age": 22,
-        "Qualification": "BCA",
-        "password": "67890"
-    },
-    3: {
-        "name": "Priya",
-        "age": 20,
-        "Qualification": "BSc",
-        "password": "11111"
-    }
-}
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
 
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
 
-# =========================
-# REQUEST MODELS
-# =========================
-
-# Model for POST request
-class Student(BaseModel):
-    name: str
-    age: int
-    Qualification: str
-    password: str
-
-
-
-# Model for PUT request
-class UpdateStudent(BaseModel):
-    name: Optional[str] = None
-    age: Optional[int] = None
-    Qualification: Optional[str] = None
-    password: Optional[str] = None
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obese'
+        
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male', 'female']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
 
 
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
 
-# =========================
-# RESPONSE MODEL
-# =========================
+    return data
 
-# This decides what API will return
-# Password will not be returned
+def save_data(data):
+    with open('patients.json', 'w') as f:
+        json.dump(data, f)
+        
 
-class StudentResponse(BaseModel):
-    name: str
-    age: int
-    Qualification: str
-
-
-
-# Home API
 @app.get("/")
-def home():
-    return {"message": "Hello World"}
+def hello():
+    return {'message':'Patient Management System API'}
+
+@app.get('/about')
+def about():
+    return {'message': 'A fully functional API to manage your patient records'}
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    # load all the patients
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail='Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='sort in asc or desc order')):
+
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select between asc and desc')
+    
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
+
+@app.post('/create')
+def create_patient(patient: Patient):
+
+    # load existing data
+    data = load_data()
+
+    # check if the patient already exists
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail='Patient already exists')
+
+    # new patient add to the database
+    data[patient.id] = patient.model_dump(exclude=['id'])
+
+    # save into the json file
+    save_data(data)
+
+    return JSONResponse(status_code=201, content={'message':'patient created successfully'})
 
 
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, patient_update: PatientUpdate):
 
-# =========================
-# GET with response_model
-# =========================
+    data = load_data()
 
-@app.get(
-    "/get-students/{students_id}",
-    response_model=StudentResponse
-)
-def get_students(
-    students_id: int = Path(..., description="ID of student")
-):
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    existing_patient_info = data[patient_id]
 
-    if students_id not in students:
-        raise HTTPException(
-            status_code=404,
-            detail="Student not found"
-        )
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
 
-    return students[students_id]
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value
 
+    #existing_patient_info -> pydantic object -> updated bmi + verdict
+    existing_patient_info['id'] = patient_id
+    patient_pydandic_obj = Patient(**existing_patient_info)
+    #-> pydantic object -> dict
+    existing_patient_info = patient_pydandic_obj.model_dump(exclude='id')
 
+    # add this dict to data
+    data[patient_id] = existing_patient_info
 
-# Get student by name
+    # save data
+    save_data(data)
 
-@app.get(
-    "/get-by-name",
-    response_model=StudentResponse
-)
-def get_student(name: str):
+    return JSONResponse(status_code=200, content={'message':'patient updated'})
 
-    for student_id in students:
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
 
-        if students[student_id]["name"].lower() == name.lower():
-            return students[student_id]
+    # load data
+    data = load_data()
 
-    raise HTTPException(
-        status_code=404,
-        detail="Student not found"
-    )
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    del data[patient_id]
 
+    save_data(data)
 
-
-# =========================
-# POST with response_model
-# =========================
-
-@app.post(
-    "/create-student/{student_id}",
-    response_model=StudentResponse
-)
-def create_student(student_id: int, student: Student):
-
-    if student_id in students:
-        raise HTTPException(
-            status_code=400,
-            detail="Student already exists"
-        )
-
-    students[student_id] = student.dict()
-
-    return students[student_id]
-
-
-
-# =========================
-# PUT with response_model
-# =========================
-
-@app.put(
-    "/update-student/{student_id}",
-    response_model=StudentResponse
-)
-def update_student(student_id: int, student: UpdateStudent):
-
-    if student_id not in students:
-        raise HTTPException(
-            status_code=404,
-            detail="Student not found"
-        )
-
-
-    if student.name is not None:
-        students[student_id]["name"] = student.name
-
-
-    if student.age is not None:
-        students[student_id]["age"] = student.age
-
-
-    if student.Qualification is not None:
-        students[student_id]["Qualification"] = student.Qualification
-
-
-    if student.password is not None:
-        students[student_id]["password"] = student.password
-
-
-    return students[student_id]
-
-
-
-# Delete API does not need response_model
-
-@app.delete("/delete-student/{student_id}")
-def delete_student(student_id: int):
-
-    if student_id not in students:
-        raise HTTPException(
-            status_code=404,
-            detail="Student not found"
-        )
-
-    del students[student_id]
-
-    return {
-        "message": "Student deleted successfully"
-    }
+    return JSONResponse(status_code=200, content={'message':'patient deleted'})
